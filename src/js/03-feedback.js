c57ec6d9794ab0775800e95cc98c092943fc917d
OksanaZhmurko <oksanazhmurko@gmail.com>
2023-11-22 01:54:02 +0200

    change code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,37 @@
-import throttle from "lodash.throttle";
-const saveFormDataToLocalStorage = throttle(() => {
-    const formData = {
-      email: document.querySelector('input[name="email"]').value,
-      message: document.querySelector('textarea[name="message"]').value,
-    };
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-  }, 500); // Ограничение вызова функции не чаще, чем раз в 500 мс
-  
-  // Функция для заполнения формы данными из локального хранилища при загрузке страницы
-  const loadFormDataFromLocalStorage = () => {
-    const storedFormData = localStorage.getItem('feedback-form-state');
-    if (storedFormData) {
-      const parsedData = JSON.parse(storedFormData);
-      document.querySelector('input[name="email"]').value = parsedData.email;
-      document.querySelector('textarea[name="message"]').value = parsedData.message;
-    }
-  };
-  
-  // Функция для обработки отправки формы
-  const handleSubmit = (event) => {
-    event.preventDefault(); // Внимание на исправление этой строки!
-  
-    const formData = {
-      email: document.querySelector('input[name="email"]').value,
-      message: document.querySelector('textarea[name="message"]').value,
-    };
-  
-    console.log(formData);
-    localStorage.removeItem('feedback-form-state');
-    document.querySelector('.feedback-form').reset();
-  };
-  
-  // Добавляем обработчики событий
-  document.addEventListener('DOMContentLoaded', () => {
-    loadFormDataFromLocalStorage(); // Загрузка данных из локального хранилища при загрузке страницы
-  
-    const form = document.querySelector('.feedback-form');
-    form.addEventListener('input', () => {
-      saveFormDataToLocalStorage(); // Сохранение данных в локальное хранилище при вводе
-    });
-  
-    form.addEventListener('submit', handleSubmit); // Обработка отправки формы
-  });
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInputData(event) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
+
+  if (email.value === '' || message.value === '') {
+    return alert(`Пожалуйста, заполните все поля!`);
+  }
+
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+  dataForm = {};
+}
